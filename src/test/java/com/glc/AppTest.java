2b8e2053d00c346ba7fe3b3acbfd282b152de08c
sarahp27 <sarah.patel@xloopdigital.com>
2023-04-09 11:37:33 -0700

    Failed Test Case 2

--- src/test/java/com/glc/AppTest.java
+++ src/test/java/com/glc/AppTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 
 public class AppTest 
 {
+    //Test case 1: Given that I visit the site, when I first start, I expect my list to be empty.
+
     @Test
     public void EmptyList()
     {
@@ -18,4 +20,20 @@ public class AppTest
         //assert
         assertEquals(list, result);
     }
+
+    // Test Case 2: Given that I have an empty list, when I add the first book to my list,
+    // ten I expect numberRead to return 1.
+
+    @Test
+    public void FirstBook()
+    {
+        int list=1;
+        Book book=new Book("The Hobbit by J.R.R", "Tolkein", 320, 1937);
+        ReadList read=new ReadList("May 22, 1992",5);
+        read.addBook(read,book1);   
+        int result=read.numberRead();
+        assertEquals(list, result);    
+    }
+
+
 }
